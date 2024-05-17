92f09a8ad1c5b8aac17226ffbb12dad8d76480d6
Axel Garcia <a22311061uthermosillo.edu.mx>
2024-05-17 00:09:07 -0700

    pre - working on SearchBar.tsx

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,6 +1,17 @@
+import styles from '../styles/searchBar.module.css';
+import iconArrow from '../assets/icon-arrow.svg';
 
 export const SearchBar = () => {
-  return (
-    <input type="text" />
-  )
+    return (
+        <div className={styles.searchBar}>
+            <input
+                className={styles.searchInput}
+                type="text"
+                placeholder="Search for any IP address or domain"
+            />
+            <button>
+                <img src={iconArrow} alt="Search button" />
+            </button>
+        </div>
+    )
 }
