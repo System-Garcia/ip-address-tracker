5aa8b7ca5c8504aadf43de928b5896937d3410be
Axel Garcia <a22311061@uthermosillo.edu.mx>
2024-09-04 22:52:58 -0700

    styles - change css names for InfoCard.tsx

--- src/components/InfoCard.tsx
+++ src/components/InfoCard.tsx
@@ -1,25 +1,24 @@
 import { InfoItem } from "./InfoItem";
-import styles from '../styles/infoCard.module.css';
+import styles from "../styles/infoCard.module.css";
 import classNames from "classnames";
 import { useContext } from "react";
 import { IpInfoContext } from "../context/IpContext";
 
 interface Props {
-    className?: string;
+  className?: string;
 }
 
 export const InfoCard = ({ className }: Props) => {
+  const { ip, isp, location } = useContext(IpInfoContext);
 
-    const {ip, isp, location } = useContext(IpInfoContext);
-
-    return (
-        <>
-            <div className={classNames(styles.infoCard, className)}>
-                <InfoItem label="ip address" value={ip || 'No result'} />
-                <InfoItem label="location" value={location.country || 'No result'} /> 
-                <InfoItem label="timezone" value={location.timezone || 'No result'} />
-                <InfoItem label="isp" value={isp || 'No result'} />
-            </div>
-        </>
-    )
-}
+  return (
+    <>
+      <div className={classNames(styles.card, className)}>
+        <InfoItem label="ip address" value={ip || "No result"} />
+        <InfoItem label="location" value={location.country || "No result"} />
+        <InfoItem label="timezone" value={location.timezone || "No result"} />
+        <InfoItem label="isp" value={isp || "No result"} />
+      </div>
+    </>
+  );
+};
