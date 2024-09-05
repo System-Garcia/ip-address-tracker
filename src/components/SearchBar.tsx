a6ba7563ba6e74891c143245901c79b9d7ce9fb6
Axel Garcia <a22311061@uthermosillo.edu.mx>
2024-09-04 23:02:45 -0700

    styles - change css names for SearchBar.tsx

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,46 +1,39 @@
-import styles from '../styles/searchBar.module.css';
-import iconArrow from '../assets/icon-arrow.svg';
-import { useForm } from '../hooks/useForm';
-import { FormEvent } from 'react';
+import styles from "../styles/searchBar.module.css";
+import iconArrow from "../assets/icon-arrow.svg";
+import { useForm } from "../hooks/useForm";
+import { FormEvent } from "react";
 
 interface Props {
-    onSearch: (ipAddress: string) => void;
+  onSearch: (ipAddress: string) => void;
 }
 
 export const SearchBar = ({ onSearch }: Props) => {
+  const { formState, onInputChange } = useForm({
+    ipAddress: "",
+  });
 
-    const { formState, onInputChange } = useForm({
-        ipAddress: '',
-    });
+  const onSubmit = (event: FormEvent) => {
+    event.preventDefault();
+    onSearch(formState.ipAddress);
+  };
 
-    const onSubmit = (event: FormEvent) => {
-        event.preventDefault();
-        onSearch(formState.ipAddress);
-    }
-
-    return (
-
-        <form
-            className={styles.searchBar}
-            onSubmit={onSubmit}
-        >
-            <input
-                className={styles.searchInput}
-                type="text"
-                placeholder="Search for any IP address or domain"
-                value={formState.ipAddress}
-                onChange={onInputChange}
-                name='ipAddress'
-            />
-            <button
-                type='submit'
-                className={styles.searchButton}>
-                <img
-                    className={styles.searchIcon}
-                    src={iconArrow}
-                    alt="Search button" />
-            </button>
-        </form>
-
-    )
-}
+  return (
+    <form className={styles["search-bar"]} onSubmit={onSubmit}>
+      <input
+        className={styles["search-bar__input"]}
+        type="text"
+        placeholder="Search for any IP address or domain"
+        value={formState.ipAddress}
+        onChange={onInputChange}
+        name="ipAddress"
+      />
+      <button type="submit" className={styles["search-bar__button"]}>
+        <img
+          className={styles["search-bar__icon"]}
+          src={iconArrow}
+          alt="Search button"
+        />
+      </button>
+    </form>
+  );
+};
