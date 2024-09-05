ad38505ae63fcc069c41eadb69904c12e8059bca
Axel Garcia <a22311061@uthermosillo.edu.mx>
2024-09-04 22:36:14 -0700

    styles - changes css names for app.module

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,16 @@
-import styles from './styles/app.module.css';
-import { Header } from './components'
-import { InfoCard } from './components/InfoCard'
-
+import styles from "./styles/app.module.css";
+import { Header } from "./components";
+import { InfoCard } from "./components/InfoCard";
 
 function App() {
-
   return (
     <>
       <div className={styles.app}>
         <Header />
-        <InfoCard
-          className={styles.appCustomInfoCard}
-        />
+        <InfoCard className={styles["app__custom-info-card"]} />
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
